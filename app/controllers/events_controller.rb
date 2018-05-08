5117cd614ef5a1bfaf94e45ef41627efad66477f
Stephen Thomas <stephenthomas@Stephens-MacBook-Pro.local>
2018-05-08 00:59:35 -0400

    event controller refactor

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,24 +1,22 @@
-require 'event_serializer.rb'
+#require 'event_serializer.rb'
 
 class EventsController < ApplicationController
-	# only accept json requests
-	#before_action :check_format
-
-	#def check_format
-	  #redirect_to root_url unless params[:format] == 'json' || request.headers["Accept"] =~ /json/
-	#end
-
 	def index
-		defaultDays = 7
-    	@days = params.has_key?(:days) ? params[:days].to_i: defaultDays
-    	startDate = DateTime.now
-    	endDate = startDate + @days - 1.days
-    	endDate = endDate.end_of_day
-    	@events = Event.where(:start_time => startDate..endDate)
-
+    	defaultDayCount = 7
+    	days = params.has_key?(:days) ? params[:days].to_i: defaultDayCount
+    	@events = eventsWithin(days)
     	json_data = EventSerializer.new(@events).serialized_json
     	respond_to do |format|
+            format.html
       		format.json { render json: json_data }
-    	end
-  end
+        end
+    end
+
+    def eventsWithin(days)
+        return [] if days <= 0
+        startDate = DateTime.now
+        endDate = startDate + days - 1.days
+        endDate = endDate.end_of_day
+        Event.where(:start_time => startDate..endDate)
+    end
 end
